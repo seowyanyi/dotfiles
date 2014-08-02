9c105d1f904b4c2e8523da850eb5ecdae0071e97
Yan Yi <seow.yanyi@gmail.com>
2014-08-02 20:34:49 +0800

    added cloning of my fork onto local machine

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,4 +1,4 @@
-h
+
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
 ############################
 
@@ -6,8 +6,9 @@ h
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
+zshthemesdir=~/.oh-my-zsh/themes
 files="bashrc vimrc zshrc"    # list of files/folders to symlink in homedir
-
+zshtheme="yanyi.zsh-theme"
 ##########
 
 # create dotfiles_old in homedir
@@ -27,4 +28,34 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+install_zsh () {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+    if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        https://github.com/seowyanyi/oh-my-zsh.git
+    fi
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+    #symbolic link for oh-my-zsh
+    ln -s $dir/oh-my-zsh/themes/$zshtheme $zshthemesdir
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        sudo apt-get install zsh
+        install_zsh
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
 
